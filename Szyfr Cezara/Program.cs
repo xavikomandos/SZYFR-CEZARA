28ecc4f415d205f391227a93e5d67d7e143328b3
Admin <Admin@E6410>
2022-05-13 16:59:45 +0200

    Dodaj pliki projektów.

---
+++ Szyfr Cezara/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+string text; 
+int key; 
+
+
+string kodCezara(string t, int k)
+{
+    t = t.ToUpper();
+    string encryptedText = ""; 
+    for (int i = 0; i < t.Length; i++)
+    {
+        char letter = t[i];
+        int letterCode = (int)letter;
+        letterCode += k;
+        letterCode = ((letterCode - 65) % 26) + 65;
+        char encryptedLetter = (char)letterCode;
+        encryptedText += encryptedLetter;
+    }
+    return encryptedText;
+}
+
+Console.WriteLine("Wpisz tekst który chcesz zaszyfrować");
+text = Console.ReadLine() ?? "";
+Console.WriteLine("O ile mają być przesuniete litery:");
+key = int.Parse(Console.ReadLine() ?? "");
+
+
+
+Console.WriteLine("Zaszyfrowana Wiadomość: " + kodCezara(text, key));
